6b64d798d8f620cd6bf51406d261af1def8c859c
Alex Skorulis <skorulis@gmail.com>
2021-03-15 17:33:22 +1100

    Initial Commit

---
+++ MoreNavStuff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoreNavStuff
+//
+//  Created by Alexander Skorulis on 15/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoreNavStuff/MoreNavStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreNavStuffApp.swift
+//  MoreNavStuff
+//
+//  Created by Alexander Skorulis on 15/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MoreNavStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
